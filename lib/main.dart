5bc67946dae6d7d0f2c1e53909fa69858c42be20
Kamil Matula <43967269+kamil-matula@users.noreply.github.com>
2022-10-16 17:07:04 +0200

    Add initializing Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_recommender/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
