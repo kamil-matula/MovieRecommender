b1e39e61d52b5985d71f263bc825ad28ee5f9b9b
Kamil Donda <dondakamil@gmail.com>
2022-10-24 16:50:26 +0200

    Dispose text controllers

--- lib/view/main_page/widgets/movie_dialog.dart
+++ lib/view/main_page/widgets/movie_dialog.dart
@@ -104,4 +104,12 @@ class _MovieDialogState extends State<MovieDialog> {
       ],
     );
   }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    directorController.dispose();
+    yearController.dispose();
+    super.dispose();
+  }
 }
