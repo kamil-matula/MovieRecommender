2f21e9d2d703beea9df802117a73bfb61edf4198
kamil-matula <mkamil1999@gmail.com>
2022-10-27 11:42:18 +0200

    Add rendering list of movies from Firebase

--- lib/view/main_page/widgets/admin_movies_tab.dart
+++ lib/view/main_page/widgets/admin_movies_tab.dart
@@ -3,30 +3,6 @@ import 'package:movie_recommender/models/movie.dart';
 import 'package:movie_recommender/view/main_page/widgets/list_of_movies.dart';
 import 'package:movie_recommender/view/movie_dialog/movie_dialog.dart';
 
-// TODO: This is temporary list only
-final List<Movie> temporaryMovies = [
-  Movie(
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    director: 'Peter Jackson',
-    genre: 'Fantasy',
-    year: 2001,
-  ),
-  Movie(
-    title: 'The Lord of the Rings: The Two Towers',
-    director: 'Peter Jackson',
-    genre: 'Fantasy',
-    year: 2002,
-    url:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
-  ),
-  Movie(
-    title: 'The Lord of the Rings: The Return of the King',
-    director: 'Peter Jackson',
-    genre: 'Fantasy',
-    year: 2003,
-  ),
-];
-
 class AdminMoviesTab extends StatelessWidget {
   final AppBar appBar;
 
@@ -39,7 +15,7 @@ class AdminMoviesTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
-      body: ListOfMovies(movies: temporaryMovies),
+      body: const ListOfMovies(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           showDialog(
